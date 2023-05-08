2d0b439d544070dd02635048a8ab6f5505d8f0de
divineion <najima.guermoudi@gmail.com>
2023-05-08 17:58:34 +0200

    fix/main: ajout du background ombre, recentrage de l'image en version mobile

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,11 @@
 const sections = document.getElementsByClassName('section');
-const reponse = document.getElementsByClassName("answer");
-
-        //LE BLOC DE CODE QUI T'INTERESSE
 for (let i=0;i<sections.length;i++) {
                 sections[i].addEventListener('click', function() {
                         const answer = sections[i].lastElementChild;
                         answer.classList.toggle('open');
+                        const img = answer.previousElementSibling.lastElementChild;   
+                        img.classList.toggle('open');       
+             
                 })
   };
 
